97616b01cdf6f0ad86438b3adbf33f7e344ad2ef
Ramya.U <ramyayadav2016@gmail.com>
2022-11-21 21:53:42 +0530

    added category class to the domain project

---
+++ StackOverflow/StackOverFlow.DomainModels/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StackOverFlow.DomainModels
+{
+    public class Category
+    {
+        //identity insert
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CategoryId { get; set; }
+
+        public string CategoryName { get; set; }
+    }
+}
