848623f8848f4e492b025af2bb61d0acbe720ce1
Ramya.U <ramyayadav2016@gmail.com>
2022-11-21 22:13:55 +0530

    added vote class to the domain project

---
+++ StackOverflow/StackOverFlow.DomainModels/Vote.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StackOverFlow.DomainModels
+{
+    public class Vote
+    {
+        //identity insert
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int VoteId { get; set; }
+
+        public int UserId { get; set; }
+
+        public int AnswerId { get; set; }
+
+        public int VoteValue { get; set; }
+    }
+}
