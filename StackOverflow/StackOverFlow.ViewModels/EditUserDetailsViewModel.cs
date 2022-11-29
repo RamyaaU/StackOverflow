faf1b30db9bacab43414f798099f549bec356d5e
Ramya.U <ramyayadav2016@gmail.com>
2022-11-29 12:43:50 +0530

    added edit password and user details view models

---
+++ StackOverflow/StackOverFlow.ViewModels/EditUserDetailsViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace StackOverFlow.ViewModels
+{
+    public class EditUserDetailsViewModel
+    {
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Mobile { get; set; }
+    }
+}
