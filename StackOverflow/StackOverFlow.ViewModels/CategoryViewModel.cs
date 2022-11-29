fd1636aa97354894e8dc58a0c2944a8bfeb7a2c1
Ramya.U <ramyayadav2016@gmail.com>
2022-11-29 12:47:14 +0530

    added category view model

---
+++ StackOverflow/StackOverFlow.ViewModels/CategoryViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace StackOverFlow.ViewModels
+{
+    public class CategoryViewModel
+    {
+        [Required]
+        public int CategoryId { get; set; }
+
+        [Required]
+        public string CategoryName { get; set; }
+    }
+}
