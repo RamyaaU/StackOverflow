dbf42aefafba05ee5b835c3abbc2024f50e605c7
Ramya.U <ramyayadav2016@gmail.com>
2022-11-29 12:32:34 +0530

    addedregister view model

---
+++ StackOverflow/StackOverFlow.ViewModels/RegisterViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverFlow.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Email { get; set; }
+
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Mobile { get; set; }
+    }
+}
