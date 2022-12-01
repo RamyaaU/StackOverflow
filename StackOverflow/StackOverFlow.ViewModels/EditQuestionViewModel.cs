06634f9ed5c268a4d158ad784ab17c0d4d1a3488
Ramya.U <ramyayadav2016@gmail.com>
2022-12-01 15:16:26 +0530

    added missed out files

---
+++ StackOverflow/StackOverFlow.ViewModels/EditQuestionViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace StackOverFlow.ViewModels
+{
+    public class EditQuestionViewModel
+    {
+        [Required]
+        public int QuestionId { get; set; }
+
+        [Required]
+        public string QuestionName { get; set; }
+
+        [Required]
+        public DateTime QuestionDateAndTime { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+    }
+}
